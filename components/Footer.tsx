0985362f44b6fbbc409a915d50a6e7aa7a39cba9
Arman Patel <vanixgaming634@gmail.com>
2024-02-28 21:36:19 +0530

    Complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { BsTelephone } from 'react-icons/bs'
 
 const Footer = () => {
   return (
     <div className='w-full p-20 flex justify-between items-center bg-red-500'>
-        <div className='flex'></div>
+        <div className='flex'>
+            <BsTelephone size={30} className='text-red-500  bg-white rounded-full'/>
+        </div>
     </div>
   )
 }
