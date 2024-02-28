39fdc5736ce93fc3ae6478d651eaeedec8b3b2f8
Arman Patel <vanixgaming634@gmail.com>
2024-02-28 21:51:17 +0530

    footer complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,38 @@
+import Link from "next/link";
 import React from "react";
-import { BsTelephone } from "react-icons/bs";
+import { BsTelephone, BsTelephoneFill } from "react-icons/bs";
+import { FaGlobe } from "react-icons/fa";
+import { RiFacebookFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
-    <div className="w-full p-20 flex justify-between items-center bg-red-500">
-      <div className="flex">
-        <div className = 'rounded-full bg-white p-10'>
-          <BsTelephone
-            size={30}
+    <div className="w-full p-20 flex flex-wrap gap-12 justify-between items-center bg-red-500">
+      <div className="flex items-center space-x-6">
+        <div className = 'rounded-full bg-white p-2'>
+          <BsTelephoneFill
+            size={20}
             className="text-red-500 "
           />
         </div>
+        <p className="text-white">Toll Free {' '}<span className="font-bold">1800 200 1234</span></p>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className = 'rounded-full bg-white p-2'>
+          <RiFacebookFill
+            size={20}
+            className="text-red-500 "
+          />
+        </div>
+        <Link href={'www.facebook.com/cripumps'} className="text-white">www.facebook.com/cripumps</Link>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className = ''>
+          <FaGlobe
+            size={26}
+            className="text-white "
+          />
+        </div>
+        <Link href={'www.crigroups.com'} className="text-white">www.crigroups.com</Link>
       </div>
     </div>
   );
